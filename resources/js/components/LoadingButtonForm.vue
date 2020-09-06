2ce52e6fcb876ff05d73d9167b142fd529d29dee
Henry Lee <henryleeworld@gmail.com>
2020-09-07 00:03:54 +0800

    feat:初始化

---
+++ resources/js/components/LoadingButtonForm.vue
@@ -0,0 +1,91 @@
+<template>
+    <form>
+        <div class="form-group">
+            <label for="name">姓名</label>
+            <input
+                type="text"
+                class="form-control"
+                id="name"
+                v-model="data.name"
+                :class="{ 'is-invalid': errors.hasOwnProperty('name') }"
+            >
+            <span class="invalid-feedback" v-if="errors.hasOwnProperty('name')">
+                {{ errors.name[0] }}
+            </span>
+        </div>
+        <div class="form-group">
+            <label for="email">電子郵件地址</label>
+            <input
+                type="email"
+                class="form-control"
+                id="email"
+                v-model="data.email"
+                :class="{ 'is-invalid': errors.hasOwnProperty('email') }"
+            >
+            <span class="invalid-feedback" v-if="errors.hasOwnProperty('email')">
+                {{ errors.email[0] }}
+            </span>
+        </div>
+
+        <div class="form-group">
+            <label for="question">問題</label>
+            <textarea
+                class="form-control"
+                :class="{ 'is-invalid': errors.hasOwnProperty('question') }"
+                id="question"
+                v-model="data.question"
+            ></textarea>
+            <span class="invalid-feedback" v-if="errors.hasOwnProperty('question')">
+                {{ errors.question[0] }}
+            </span>
+        </div>
+
+        <VueLoadingButton
+            :styled="false"
+            :loading="isLoading"
+            @click.native="submitForm"
+            class='h6 btn btn-sm btn-primary'
+        ></VueLoadingButton>
+
+        <div class="alert alert-success my-2" v-if="isSuccess">
+            電子郵件發送成功
+        </div>
+    </form>
+</template>
+
+<script>
+import VueLoadingButton from 'vue-loading-button'
+
+export default {
+    components: {
+        VueLoadingButton,
+    },
+    data () {
+        return {
+            isLoading: false,
+            isSuccess: false,
+            data: {
+                name: '',
+                email: '',
+                question: '',
+            },
+            errors: {}
+        }
+    },
+    methods: {
+        submitForm () {
+            this.errors = {};
+            this.isLoading = true;
+            this.isSuccess = false;
+            axios.post('/submit', this.data)
+                .then(() => this.isSuccess = true)
+                .catch(({response}) => {
+                    if (response.status === 422) {
+                        this.errors = response.data.errors;
+                    }
+                })
+                .finally(() => this.isLoading = false);
+        }
+    }
+}
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">{{ trans('frontend.contact_us.title') }}</div>
+
+                <div class="card-body" id="app">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <loading-button-form></loading-button-form>
+                        </div>
+                        <div class="col-md-6">
+                            <loading-overlay-form></loading-overlay-form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
